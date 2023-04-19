4ff8bd65cee2b6e34b2320e2cb78c06e05b406e1
wfly <953212389@qq.com>
2023-04-19 15:23:20 +0800

    fix: delete useless code

--- src/apis/request.ts
+++ src/apis/request.ts
@@ -40,7 +40,6 @@ export const request = createAxios({
           await userStore.logout();
         }
 
-        // VriMessage.error(msg);
         // const cancelRequest = new RequestCanceler();
         // const pendMap = cloneDeep(cancelRequest.getAllPending());
         // cancelRequest.removeAllPending();
