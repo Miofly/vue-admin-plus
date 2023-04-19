34652f3f72985b51109d7a55f54f185fee8440f1
wfly <953212389@qq.com>
2023-04-19 15:29:42 +0800

    feat: add useTab test

--- src/views/business/demo/index.vue
+++ src/views/business/demo/index.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
+import { useTabs } from '@vft/store';
 
+const {
+  refreshPage,
+  closeAll,
+  setTitle
+} = useTabs();
 </script>
 
 <template>
-	index
+  <vft-button @click="refreshPage()">refreshPage</vft-button>
+  <vft-button @click="closeAll()">closeAll</vft-button>
+  <vft-button @click="setTitle('自定义title')">setTitle</vft-button>
 </template>
 
 <style scoped lang="scss">
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,31 +1,5 @@
-<script setup lang="ts">
-import { useTabs } from '@vft/store';
-
-const {
-  refreshPage,
-  closeAll,
-  closeLeft,
-  closeRight,
-  closeOther,
-  closeCurrent,
-  // close,
-  setTitle
-} = useTabs();
-
-function handleClose() {
-  // close({ fullPath: '/style/scss/mixinInclude' } as RouteLocationNormalized);
-}
-</script>
-
 <template>
-  <vft-button @click="refreshPage()">refreshPage</vft-button>
-  <vft-button @click="closeAll()">closeAll</vft-button>
-  <vft-button @click="closeLeft()">closeLeft</vft-button>
-  <vft-button @click="closeRight()">closeRight</vft-button>
-  <vft-button @click="closeOther()">closeOther</vft-button>
-  <vft-button @click="closeCurrent()">closeCurrent</vft-button>
-  <vft-button @click="handleClose">close</vft-button>
-  <vft-button @click="setTitle('自定义title')">setTitle</vft-button>
+  主页
 </template>
 
 <style scoped lang="scss">
