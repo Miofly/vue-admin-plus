8bcf61b8ba8d92c2d90a5dbde5e20660a7f2f953
wfly <953212389@qq.com>
2023-04-20 13:23:40 +0800

    fix: add env ts to resolve vscode vue error

---
+++ types/env.ts
@@ -0,0 +1,12 @@
+// @ts-nocheck
+// @ts-ignore
+declare module '*.vue' {
+  import { DefineComponent } from 'vue';
+  const Component: DefineComponent<{}, {}, any>;
+  export default Component;
+}
+
+declare module 'virtual:*' {
+  const result: any;
+  export default result;
+}
