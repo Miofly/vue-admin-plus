b9fbcf8703904dd67fdab43849bcdad2034a5046
wfly <953212389@qq.com>
2023-04-28 16:14:11 +0800

    perf(drag): add move style

--- admin/src/views/home/index.vue
+++ admin/src/views/home/index.vue
@@ -3,7 +3,7 @@ const initArr = reactive([
   { car: 'active bg-blue-100', bit: [5, 6, 7, 8] },
   { car: 'active bg-pink-100', bit: [61] },
   { car: 'active bg-green-100', bit: [47, 48, 49] },
-  { car: 'active bg-red-100', bit: [20, 21, 22, 23, 24] }
+  { car: 'active bg-red-100', bit: [20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34] }
 ]);
 
 const classes = computed(() => {
@@ -20,28 +20,76 @@ const classes = computed(() => {
   };
 });
 
-function handleDargEnter(data) {
+function differenceArr(arr1: any[], arr2: any[]): any {
+  // 判断参数
+  if (arr1.length === 0) {
+    return [];
+  }
+  if (arr2.length === 0) {
+    return arr1.slice();
+  }
+  return arr1.filter((item) => !arr2.includes(item));
+}
+
+const isDrag = ref(false);
+const isDragIndex = ref();
+
+function handleDragEnter(data) {
+  isDrag.value = true;
   const startIndex = Number(data.fromElement?.innerText || data.target?.innerText);
   const endIndex = Number(data.toElement.innerText);
-  if (startIndex !== endIndex) {
-    for (let i = 0; i < initArr?.length; i++) {
-      const _index = initArr[i].bit.findIndex((item) => item === startIndex);
 
+  if (startIndex !== endIndex) {
+    let otherBits: number[] = [];
+
+    for (const item of initArr) {
+      for (const subitem of item.bit) {
+        otherBits.push(subitem);
+      }
+    }
+
+    for (let i = 0; i < initArr?.length; i++) {
+
+      const _index = initArr[i].bit.findIndex((item) => item === startIndex);
       if (_index >= 0) {
-        initArr[i].bit = initArr[i].bit.map((item) => {
+        const result = differenceArr(otherBits, initArr[i].bit);
+
+        console.log(result, endIndex, result.includes(endIndex));
+        if (result.includes(endIndex)) {
+          isDrag.value = false;
+        	return;
+        }
+
+        const new_arr = initArr[i].bit.map((item) => {
           item = item + endIndex - startIndex;
           return item;
         });
+
+        const whetherToCross = new_arr.some(item => {
+          return result.includes(item);
+        });
+
+        if (!whetherToCross) {
+          isDragIndex.value = endIndex;
+
+          initArr[i].bit = new_arr;
+        }
         return;
       }
     }
   }
 }
+
+function handleDragEnd () {
+  isDrag.value = false;
+}
 </script>
 
 <template>
   <div class="container">
-    <div :class="classes(item)" draggable="true" @dragenter="handleDargEnter"
+    <div :class="classes(item)" :draggable="!isDrag"
+      @dragenter.prevent="handleDragEnter"
+      @dragend="handleDragEnd"
       v-for="(item, index) in 64" :key="index">
       {{ item }}
     </div>
